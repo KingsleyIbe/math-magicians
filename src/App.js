589d9656daff02221d34f328db4dd91876ade5ed
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-04 14:11:54 +0100

    Add css for ui

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Calculator from './Components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Math magicians</h1>
-      </header>
+    <div>
       <Calculator />
     </div>
   );
