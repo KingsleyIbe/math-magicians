746c5ae4eabcc5266c0bf0632979dcb9a943e9ba
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-05 16:28:17 +0100

    Update readme file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculator from './Components/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root'),
 );
