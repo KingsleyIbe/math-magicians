b08f10b11fc7369f948e7670498a6d78bf071a93
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-12 00:29:24 +0100

    Implement changes as recommended by the reviewer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Components/Router';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Calculator from './Components/Calculator';
-// import Header from './Components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Router basename={process.env.PUBLIC_URL}> */}
-    {/* <Header /> */}
-    {/* <Routes> */}
-    {/* <Route /> */}
-    {/* </Routes> */}
-    {/* <Calculator /> */}
-    {/* </Router> */}
     <Router />
   </React.StrictMode>,
   document.getElementById('root'),
