bed94999a4361117bef0fb5554ee0e4ced5e038a
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-12 12:04:34 +0100

    Initial commit on the branch

--- src/App.test.js
+++ src/App.test.js
@@ -5,9 +5,9 @@ describe('My math function', () => {
   test.each`
   numberOne | numberTwo | operation | expected
   ${4}      | ${4}      | ${'+'}    | ${8}
-  ${7}      | ${4}      | ${'-'}    | ${0}
-  ${8}      | ${4}      | ${'/'}    | ${1}
-  ${3}      | ${4}      | ${'*'}    | ${16}
+  ${7}      | ${4}      | ${'-'}    | ${3}
+  ${8}      | ${4}      | ${'/'}    | ${2}
+  ${3}      | ${4}      | ${'*'}    | ${12}
   `('adds $numberOne + $numberTwo to equal $expected', ({
     numberOne, numberTwo, operation, expected,
   }) => {
